554fe7bc8b1a517297f62c79aa7a8e68e4ea295d
khalliid9 <kvadah9@gmail.com>
2024-12-04 09:49:32 +0300

    update register view UI

--- lib/Register.dart
+++ lib/Register.dart
@@ -37,63 +37,112 @@ class _RegisterviewState extends State<Registerview> {
         title: const Text('Register'),
         backgroundColor: Colors.lightBlue,
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: _email,
-            keyboardType: TextInputType.emailAddress,
-            enableSuggestions: false,
-            autocorrect: false,
-            decoration: const InputDecoration(
-              hintText: ('Email'),
-            ),
-          ),
-          TextField(
-            controller: _password,
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            decoration: const InputDecoration(
-              hintText: ('Password'),
-            ),
-          ),
-          TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.lightBlue,
+      body: Container(
+        margin: EdgeInsets.only(left: 10, right: 10),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              child: Text(
+                'Sign Up',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 55, 241, 49)
+                ),
               ),
-              onPressed: () async {
-                final email = _email.text;
-                final password = _password.text;
-                try {
-                  await AuthService.firebase()
-                      .createUser(email: email, password: password);
-                  AuthService.firebase().sendVertificationEmail();
-                  Navigator.of(context).pushNamed('/verifyEmail');
-                } on EmailAlreadyInUseAuthException {
-                  await showErrorDialog(
-                    context,
-                    'this Email is already Registerd',
-                  );
-                } on InvalidEmailAuthException {
-                  await showErrorDialog(
-                    context,
-                    'Invalid Email Format',
-                  );
-                } on WeakPasswordAuthException {
-                  await showErrorDialog(
-                    context,
-                    'Use Strong Password',
-                  );
-                }
-              },
-              child: const Text('Register')),
-          TextButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil('/login', (route) => false);
-              },
-              child: const Text('already registered? login here'))
-        ],
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 11),
+              child: TextField(
+                controller: _email,
+                keyboardType: TextInputType.emailAddress,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.black,
+                  ),
+                  hintText: ('Email'),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(7),
+                    borderSide: BorderSide(
+                      color: Colors.blue, // Border color
+                      width: 1.1, // Border thickness
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 11),
+              child: TextField(
+                controller: _password,
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.black,
+                  ),
+                  hintText: ('Password'),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(7),
+                    borderSide: BorderSide(
+                      color: Colors.blue, // Border color
+                      width: 1.1, // Border thickness
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8))),
+                  onPressed: () async {
+                    final email = _email.text;
+                    final password = _password.text;
+                    try {
+                      await AuthService.firebase()
+                          .createUser(email: email, password: password);
+                      AuthService.firebase().sendVertificationEmail();
+                      Navigator.of(context).pushNamed('/verifyEmail');
+                    } on EmailAlreadyInUseAuthException {
+                      await showErrorDialog(
+                        context,
+                        'this Email is already Registerd',
+                      );
+                    } on InvalidEmailAuthException {
+                      await showErrorDialog(
+                        context,
+                        'Invalid Email Format',
+                      );
+                    } on WeakPasswordAuthException {
+                      await showErrorDialog(
+                        context,
+                        'Use Strong Password',
+                      );
+                    }
+                  },
+                  child: const Text('Register')),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 18),
+              child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/login', (route) => false);
+                  },
+                  child: const Text('already registered? login here')),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/Views/login.dart
+++ lib/Views/login.dart
@@ -48,113 +48,113 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        backgroundColor: Colors.lightBlue,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(bottom: 40),
-              height: 100,
-              width: 100,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                shape: BoxShape.circle,
-              ),
-              child: const Icon(
-                Icons.note,
-                color: Colors.white,
-                size: 50,
-              ),
+    appBar: AppBar(
+      title: const Text('Login'),
+      backgroundColor: Colors.lightBlue,
+    ),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(bottom: 40),
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              shape: BoxShape.circle,
             ),
-            // email text field
-            Container(
-              margin: EdgeInsets.only(bottom: 10, right: 15, left: 15),
-              child: TextField(
-                controller: _email,
-                keyboardType: TextInputType.emailAddress,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.email, color: Colors.black),
-                  hintText: ('Email'),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      color: Colors.black, // Border color
-                      width: 1.1, // Border thickness
-                    ),
-                  ),
+            child: const Icon(
+              Icons.note,
+              color: Colors.white,
+              size: 50,
+            ),
+          ),
+          // email text field
+        Container(
+          margin: EdgeInsets.only(bottom: 10, right: 15, left: 15),
+          child: TextField(
+            controller: _email,
+            keyboardType: TextInputType.emailAddress,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(Icons.email, color: Colors.black),
+              hintText: ('Email'),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(
+                  color: Colors.black, // Border color
+                  width: 1.1, // Border thickness
                 ),
               ),
             ),
-            //password text field
-            Container(
-              margin: const EdgeInsets.only(bottom: 10, right: 15, left: 15),
-              child: TextField(
-                controller: _password,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.lock,
-                    color: Colors.black,
-                  ),
-                  hintText: ('Password'),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      color: Colors.black, // Border color
-                      width: 1.1, // Border thickness
-                    ),
-                  ),
+          ),
+        ),
+        //password text field
+        Container(
+          margin: const EdgeInsets.only(bottom: 10, right: 15, left: 15),
+          child: TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.black,
+              ),
+              hintText: ('Password'),
+              border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                  color: Colors.black, // Border color
+                  width: 1.1, // Border thickness
                 ),
               ),
             ),
-            //login text Button
-            SizedBox(
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    backgroundColor: Colors.blue,
-                  ),
-                  onPressed: () async {
-                    final email = _email.text;
-                    final password = _password.text;
+          ),
+        ),
+        //login text Button
+        SizedBox(
+          child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                backgroundColor: Colors.blue,
+              ),
+              onPressed: () async {
+                final email = _email.text;
+                final password = _password.text;
 
-                    try {
-                      // signin user in firebase  with inputed email and password
-                      // final currentUser = AuthService.firebase().currentUser;
-                      await AuthService.firebase().login(
-                        email: email,
-                        password: password,
-                      );
+                try {
+                  // signin user in firebase  with inputed email and password
+                  // final currentUser = AuthService.firebase().currentUser;
+                  await AuthService.firebase().login(
+                    email: email,
+                    password: password,
+                  );
 
-                      if (mounted) {
-                        Navigator.of(context).pushNamedAndRemoveUntil(
-                          '/notes',
-                          (route) => false,
-                        );
-                      }
-                    } on InvalidEmailAuthException {
-                      await showErrorDialog(context, 'Invalid Email');
-                    } on InvalidCredentialAuthException {
-                      await showErrorDialog(context, "Wrong Email or Password");
-                    } on GenericAuthException {
-                      await showErrorDialog(context, "Failed to login");
-                    }
-                  },
-                  child: const Text('Login')),
-            ),
-            // text button to navigate to a register view
-            TextButton(
+                  if (mounted) {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      '/notes',
+                      (route) => false,
+                    );
+                  }
+                } on InvalidEmailAuthException {
+                  await showErrorDialog(context, 'Invalid Email');
+                } on InvalidCredentialAuthException {
+                  await showErrorDialog(context, "Wrong Email or Password");
+                } on GenericAuthException {
+                  await showErrorDialog(context, "Failed to login");
+                }
+              },
+              child: const Text('Login')),
+        ),
+        // text button to navigate to a register view
+        TextButton(
                 onPressed: () {
                   Navigator.of(context)
                       .pushNamedAndRemoveUntil('/register', (route) => false);
